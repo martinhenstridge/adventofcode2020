9fa4d11d62ecf8b73365aa9385f78e68f5a0ac8f
Martin Henstridge <mhenstri@cisco.com>
2020-12-03 07:53:10 -0800

    Solved 3.1

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -1,9 +1,11 @@
 from . import (
     day01,
     day02,
+    day03,
 )
 
 DAYS = {
     "01": day01,
     "02": day02,
+    "03": day03,
 }
---
+++ aoc/day03.py
@@ -0,0 +1,42 @@
+from . import util
+
+
+class Grid:
+    def __init__(self, grid):
+        self.grid = grid
+        self.ymax = len(grid)
+        self.xmax = len(grid[0])
+
+    def step(self, start, delta):
+        x0, y0 = start
+        dx, dy = delta
+
+        x1 = (x0 + dx) % self.xmax
+        y1 = y0 + dy
+
+        return x1, y1
+
+    def walk(self, dx, dy):
+        locations = []
+        x = 0
+        y = 0
+
+        while y < self.ymax:
+            locations.append((x, y))
+            x, y = self.step((x, y), (dx, dy))
+
+        return locations
+
+    def is_tree(self, x, y):
+        return self.grid[y][x] == "#"
+
+    def tree_count(self, dx, dy):
+        return sum(1 for x, y in self.walk(dx, dy) if self.is_tree(x, y))
+
+
+def run():
+    inputlines = util.get_input_lines("03.txt")
+    grid = Grid(inputlines)
+
+    count = grid.tree_count(3, 1)
+    print(count)
