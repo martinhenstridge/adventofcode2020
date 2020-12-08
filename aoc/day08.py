a4c2564ccb08c1b925844a6d548a9a9c88adc1eb
Martin Henstridge <mhenstri@cisco.com>
2020-12-08 10:19:16 -0800

    Tinkering with day 8

--- aoc/day08.py
+++ aoc/day08.py
@@ -21,46 +21,35 @@ def update(idx, acc, ins, arg):
 def boot(instructions):
     idx = 0
     acc = 0
+    eof = len(instructions)
     visited = set()
-    terminate = len(instructions)
 
-    while True:
-        if idx == terminate:
-            return acc, visited, True
-        if idx in visited:
-            return acc, visited, False
+    while idx not in visited and idx != eof:
         visited.add(idx)
         ins, arg = instructions[idx]
         idx, acc = update(idx, acc, ins, arg)
+    return acc, visited, idx == eof
 
 
 def run():
     inputlines = util.get_input_lines("08.txt")
     instructions = [ins for ins in get_instructions(inputlines)]
 
-    # Run the unmodified instruction list.
     acc1, visited, _ = boot(instructions)
 
-    # The faulty instruction must have been visited during part 1, try modifying
-    # each visited instruction in turn.
+    # The faulty instruction must have been visited during part 1 above.
     for idx in sorted(visited):
-        # Modify the instruction as appropriate. Skip any 'nop' instructions
-        # with an argument of 0 - translated into a 'jmp this is an immediate
-        # infinite loop.
         ins, arg = instructions[idx]
+
         if ins == "jmp":
             instructions[idx] = ("nop", arg)
         elif ins == "nop" and arg != 0:
             instructions[idx] = ("jmp", arg)
         else:
             continue
-
-        # Run the modified instruction list, then replace the original
-        # instruction when finished.
         acc2, _, term = boot(instructions)
         instructions[idx] = (ins, arg)
 
-        # If the modified instruction list now terminates, we're done.
         if term:
             break
 
