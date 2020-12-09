5bce3a79ed9724b027964af29b33b856a15ba065
Martin Henstridge <mhenstri@cisco.com>
2020-12-09 05:00:32 -0800

    Add timing output

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -1,12 +1,16 @@
 import sys
+import time
 from . import DAYS
 
 
 def run(solvers, day):
     print()
+    start = time.monotonic()
     parts = solvers[day].run()
+    end = time.monotonic()
     for idx, part in enumerate(parts):
-        print(f"[{day}|{idx+1}] {part}")
+        print(f"[{day}/{idx+1}] {part}")
+    print(f"{1000 * (end - start):.3f}ms")
 
 
 if len(sys.argv) > 1:
