aa244be2c9d8917bbbeb564b29853103e9c6634e
Martin Henstridge <mhenstri@cisco.com>
2020-12-09 04:46:07 -0800

    Parameterize 9.1, and naming

--- aoc/day09.py
+++ aoc/day09.py
@@ -5,16 +5,16 @@ def get_numbers(lines):
     return [int(line) for line in lines]
 
 
-def find_invalid(numbers):
-    window = set(numbers[:25])
-    for idx, target in enumerate(numbers[25:]):
+def find_invalid(numbers, plen):
+    window = set(numbers[:plen])
+    for idx, target in enumerate(numbers[plen:]):
         if not any(target - n != n and target - n in window for n in window):
             return target
         window.remove(numbers[idx])
-        window.add(numbers[idx + 25])
+        window.add(numbers[plen + idx])
 
 
-def find_sum_sequence(numbers, target):
+def find_sequence(numbers, target):
     head = 0
     tail = 0
     total = 0
@@ -33,7 +33,7 @@ def run():
     inputlines = util.get_input_lines("09.txt")
     numbers = get_numbers(inputlines)
 
-    invalid = find_invalid(numbers)
-    sequence = find_sum_sequence(numbers, invalid)
+    invalid = find_invalid(numbers, 25)
+    sequence = find_sequence(numbers, invalid)
 
     return (invalid, min(sequence) + max(sequence))
