ecf2145930882aea480aa3207e44fc2322aaf9a5
Martin Henstridge <mhenstri@cisco.com>
2020-12-10 12:13:59 -0800

    Tidying

--- aoc/day10.py
+++ aoc/day10.py
@@ -6,21 +6,31 @@ def get_joltages(lines):
     return [int(line) for line in lines]
 
 
+def get_joltage_pairs(chain):
+    yield from zip(chain[:-1], chain[1:])
+
+
 def count_differences(chain):
     diffs = collections.defaultdict(int)
-    for a, b in zip(chain[:-1], chain[1:]):
+    for a, b in get_joltage_pairs(chain):
         diffs[b - a] += 1
     return diffs
 
 
-# Consecutive adapters in the valid chain all differ from their neighbours by
-# either 1 jolt or 3 jolts. Since the maximum allowed difference is 3 jolts, the
-# only extra valid chains of adapters are those which remove some combination of
-# adapters which differ from their neighboutrs by 1 jolt.
+# Consecutive adapters in the valid chain all differ from their immediate
+# neighbours by either 1 jolt or 3 jolts. Since the maximum allowed difference
+# is 3 jolts, the only additional valid chains of adapters are those omitting
+# some combination of adapters from a streak of adapters which all differ from
+# their neighbours by 1 jolt.
 #
-# The longest streak of consecutive joltage values is 5, and the shorted streak
-# which can possibly have at least one adapter removed is 3. For this range, the
-# possible combinations are listed below:
+# The total number of valid combinations is equal to the product of the number
+# of valid combinations for each such streak. The location and contents of each
+# streak is irrelevant, only its length is significant.
+#
+# The longest streak of consecutive joltage values from the input provided has a
+# length of 5, and the shortest streak which can possibly have at least one
+# adapter removed is of length 3. For this range, all possible permutations are
+# listed below:
 #
 # x x x
 # x . x
@@ -45,16 +55,19 @@ COMBINATIONS = {
 
 
 def find_streaks(chain):
-    streak = 0
-    for a, b in zip(chain[:-1], chain[1:]):
+    start = None
+    for idx, (a, b) in enumerate(get_joltage_pairs(chain)):
         if b - a == 1:
-            if streak == 0:
-                streak = 1
-            streak += 1
+            if start is None:
+                start = idx
         else:
-            if streak > 2:
-                yield streak
-            streak = 0
+            if start is not None and idx - start > 1:
+                yield chain[start : idx + 1]
+            start = None
+
+
+def count_combinations(chain):
+    return util.product(COMBINATIONS[len(streak)] for streak in find_streaks(chain))
 
 
 def run():
@@ -68,6 +81,6 @@ def run():
     chain = sorted(joltages)
 
     diffs = count_differences(chain)
-    combs = util.product(COMBINATIONS[streak] for streak in find_streaks(chain))
+    combs = count_combinations(chain)
 
     return (diffs[1] * diffs[3], combs)
