fbeae8450e5dfc87801120a448484355f6f082da
Martin Henstridge <mhenstri@cisco.com>
2020-12-10 11:48:41 -0800

    Solved 10.2

--- aoc/day10.py
+++ aoc/day10.py
@@ -13,6 +13,50 @@ def count_differences(chain):
     return diffs
 
 
+# Consecutive adapters in the valid chain all differ from their neighbours by
+# either 1 jolt or 3 jolts. Since the maximum allowed difference is 3 jolts, the
+# only extra valid chains of adapters are those which remove some combination of
+# adapters which differ from their neighboutrs by 1 jolt.
+#
+# The longest streak of consecutive joltage values is 5, and the shorted streak
+# which can possibly have at least one adapter removed is 3. For this range, the
+# possible combinations are listed below:
+#
+# x x x
+# x . x
+#
+# x x x x
+# x . x x
+# x x . x
+# x . . x
+#
+# x x x x x
+# x . x x x
+# x x . x x
+# x x x . x
+# x . . x x
+# x . x . x
+# x x . . x
+COMBINATIONS = {
+    3: 2,
+    4: 4,
+    5: 7,
+}
+
+
+def find_streaks(chain):
+    streak = 0
+    for a, b in zip(chain[:-1], chain[1:]):
+        if b - a == 1:
+            if streak == 0:
+                streak = 1
+            streak += 1
+        else:
+            if streak > 2:
+                yield streak
+            streak = 0
+
+
 def run():
     inputlines = util.get_input_lines("10.txt")
     joltages = get_joltages(inputlines)
@@ -24,4 +68,6 @@ def run():
     chain = sorted(joltages)
 
     diffs = count_differences(chain)
-    return (diffs[1] * diffs[3],)
+    combs = util.product(COMBINATIONS[streak] for streak in find_streaks(chain))
+
+    return (diffs[1] * diffs[3], combs)
