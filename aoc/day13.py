1ac47f9cd14d54026b8bf5d52073d8878d2ba531
Martin Henstridge <mchenstridge@gmail.com>
2020-12-13 23:04:07 +0000

    Proper solution for 13.2

--- aoc/day13.py
+++ aoc/day13.py
@@ -4,47 +4,70 @@ from . import util
 
 def get_bus_data(lines):
     earliest = int(lines[0])
-    schedule = {}
-    for idx, time in enumerate(lines[1].split(",")):
-        if time != "x":
-            schedule[int(time)] = idx
+    schedule = {i: int(t) for i, t in enumerate(lines[1].split(",")) if t != "x"}
     return earliest, schedule
-
-
-def first_multiple_at_least(minimum, factor):
-    return factor * math.ceil(minimum / factor)
 
 
 def find_first_bus(schedule, earliest):
     result = 0, math.inf
-    for bus in schedule:
-        departs = first_multiple_at_least(earliest, bus)
+
+    for bus in schedule.values():
+        departs = bus * math.ceil(earliest / bus)
         if departs < result[1]:
             result = bus, departs
+
     return result
 
 
-def find_first_timestamp(schedule, lowerbound):
-    # [439, 13, 17, 23, 41] are aligned at offset 17
-    step = 91488917
-    offset = 17
+def find_first_timestamp(schedule):
+    # Each offset:bus pair constrains the solution (t) as follows:
+    #
+    # (t + offset) % bus == 0
+    #
+    # Take the following as an example:
+    #
+    # 7,13,x,x,59,x,31,19 => { 0:7, 1:13, 4:59, 6:31, 7:19 }
+    #
+    # The 0:7 constraint is trivially satisfied by t = 0. Incrementing by 7
+    # yields an endless supply of further conforming values for subsequent use.
+    #
+    # The 1:13 constraint is satisfied by incrementing t by 7 until we encounter
+    # a value such that t+1 is exactly divisible by 13, i.e. (t + 1) % 13 == 0.
+    # The first such value is 77. Further conforming values can then be found by
+    # incrementing t by 7 * 13 == 91.
+    #
+    # Note: all bus times are prime, so we need not consider intermediate values
+    # (there are no common divisors).
+    #
+    # The 4:59 constraint is satisfied by incrementing t (== 77) by 91 until we
+    # encounter a value such that (t + 4) % 59 == 0. The first such value is
+    # 350. Further values are available at increments of 7 * 13 * 59 == 5369.
+    #
+    # Similarly, applying the 6:31 constraint yields a timestamp of 70147, with
+    # further values at increments of 7 * 13 * 59 * 31 == 166439.
+    #
+    # Finally, applying the 7:19 constraint yields the solution: 1068781.
+    timestamp = 0
+    interval = 1
 
-    schedule.pop(13)
-    schedule.pop(17)
-    schedule.pop(23)
-    schedule.pop(41)
-    print(schedule)
+    for offset, bus in schedule.items():
+        while (timestamp + offset) % bus != 0:
+            timestamp += interval
+        interval *= bus
 
-    t = first_multiple_at_least(lowerbound, step)
-    t -= offset
+    return timestamp
 
-    while True:
-        t += step
-        for bus, offset in schedule.items():
-            if (t + offset) % bus != 0:
-                break
-        else:
-            return t
+
+# EXAMPLES = {
+#    1068781: {0: 7, 1: 13, 4: 59, 6: 31, 7: 19},
+#    3417: {0: 17, 2: 13, 3: 19},
+#    754018: {0: 67, 1: 7, 2: 59, 3: 61},
+#    779210: {0: 67, 2: 7, 3: 59, 4: 61},
+#    1261476: {0: 67, 1: 7, 3: 59, 4: 61},
+#    1202161486: {0: 1789, 1: 37, 2: 47, 3: 1889},
+# }
+# for soln, schedule in EXAMPLES.items():
+#    assert find_first_timestamp(schedule) == soln
 
 
 def run():
@@ -52,6 +75,6 @@ def run():
     earliest, schedule = get_bus_data(inputlines)
 
     bus, departs = find_first_bus(schedule, earliest)
-    timestamp = find_first_timestamp(schedule, 100000000000000)
+    timestamp = find_first_timestamp(schedule)
 
     return (bus * (departs - earliest), timestamp)
