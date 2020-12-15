2d8ee61af6dc414edbfa727931c10536b081b064
Martin Henstridge <mhenstri@cisco.com>
2020-12-15 03:11:34 -0800

    Slightly faster without generator

--- aoc/day15.py
+++ aoc/day15.py
@@ -5,36 +5,27 @@ def get_starting_numbers(lines):
     return [int(n) for n in lines[0].split(",")]
 
 
-def game(starting):
+def play(starting, count):
     turns = {}
 
     for idx, num in enumerate(starting):
         turns[num] = (idx, idx)
-        yield num
 
-    idx = len(starting)
     num = starting[-1]
-    while True:
+    for idx in range(len(starting), count):
         num = turns[num][0] - turns[num][1]
         if num in turns:
             turns[num] = (idx, turns[num][0])
         else:
             turns[num] = (idx, idx)
-        yield num
-        idx += 1
-
-
-def play(generator, count):
-    for _ in range(count):
-        result = next(generator)
-    return result
+    return num
 
 
 def run():
     inputlines = util.get_input_lines("15.txt")
     starting = get_starting_numbers(inputlines)
 
-    number1 = play(game(starting), 2020)
-    number2 = play(game(starting), 30000000)
+    number1 = play(starting, 2020)
+    number2 = play(starting, 30000000)
 
     return number1, number2
