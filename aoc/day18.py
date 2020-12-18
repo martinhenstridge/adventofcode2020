d688f45953e279805d2b2ba169ea770990d06f36
Martin Henstridge <mhenstri@cisco.com>
2020-12-18 07:14:48 -0800

    Speculatively changing to one-step evaluation

--- aoc/day18.py
+++ aoc/day18.py
@@ -3,50 +3,35 @@ from . import util
 
 
 def get_expressions(lines):
-    return [parse(line.replace(" ", "")) for line in lines]
-
-
-def parse(string):
-    expr = []
-    idx = 0
-
-    while idx < len(string):
-        char = string[idx]
-        if char.isdigit():
-            expr.append(int(char))
-        elif char == "+":
-            expr.append(operator.add)
-        elif char == "*":
-            expr.append(operator.mul)
-        elif char == "(":
-            subexpr, length = parse(string[idx + 1 :])
-            expr.append(subexpr)
-            idx += length
-        elif char == ")":
-            return expr, idx + 1
-        idx += 1
-
-    return expr, idx
+    return [line.replace(" ", "") for line in lines]
 
 
 def evaluate(expr):
-    acc, expr = expr[0], expr[1:]
-    if isinstance(acc, list):
-        acc = evaluate(acc)
+    acc = 0
+    op = operator.add
 
     while expr:
-        op, val, expr = expr[0], expr[1], expr[2:]
-        if isinstance(val, list):
-            val = evaluate(val)
-        acc = op(acc, val)
+        char, expr = expr[0], expr[1:]
+        if char == "+":
+            op = operator.add
+        elif char == "*":
+            op = operator.mul
+        elif char == "(":
+            val, expr = evaluate(expr)
+            acc = op(val, acc)
+        elif char == ")":
+            return acc, expr
+        else:
+            acc = op(int(char), acc)
 
-    return acc
+
+    return acc, []
 
 
 def run():
     inputlines = util.get_input_lines("18.txt")
     expressions = get_expressions(inputlines)
 
-    total = sum(evaluate(e) for e, _ in expressions)
+    total = sum(evaluate(e)[0] for e in expressions)
 
     return (total,)
