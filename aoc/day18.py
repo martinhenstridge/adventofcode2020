e52d33b5abf0759a21c9d1ecb43ba16816c2d987
Martin Henstridge <mhenstri@cisco.com>
2020-12-18 07:30:29 -0800

    Revert to two-step evaluation

--- aoc/day18.py
+++ aoc/day18.py
@@ -3,35 +3,47 @@ from . import util
 
 
 def get_expressions(lines):
-    return [line.replace(" ", "") for line in lines]
+    return [parse(line.replace(" ", "")) for line in lines]
+
+
+def parse(string):
+    expr = []
+
+    while string:
+        char, string = string[0], string[1:]
+        if char == "(":
+            subexpr, string = parse(string)
+            expr.append(subexpr)
+        elif char == ")":
+            return expr, string
+        elif char == "+":
+            expr.append(operator.add)
+        elif char == "*":
+            expr.append(operator.mul)
+        else:
+            expr.append(int(char))
+
+    return expr
 
 
 def evaluate(expr):
-    acc = 0
-    op = operator.add
+    acc, expr = expr[0], expr[1:]
+    if isinstance(acc, list):
+        acc = evaluate(acc)
 
     while expr:
-        char, expr = expr[0], expr[1:]
-        if char == "+":
-            op = operator.add
-        elif char == "*":
-            op = operator.mul
-        elif char == "(":
-            val, expr = evaluate(expr)
-            acc = op(val, acc)
-        elif char == ")":
-            return acc, expr
-        else:
-            acc = op(int(char), acc)
+        op, val, expr = expr[0], expr[1], expr[2:]
+        if isinstance(val, list):
+            val = evaluate(val)
+        acc = op(acc, val)
 
-
-    return acc, []
+    return acc
 
 
 def run():
     inputlines = util.get_input_lines("18.txt")
     expressions = get_expressions(inputlines)
 
-    total = sum(evaluate(e)[0] for e in expressions)
+    total = sum(evaluate(e) for e in expressions)
 
     return (total,)
