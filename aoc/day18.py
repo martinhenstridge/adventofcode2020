7194896cf6a6d698977281d1a53af52eb45a8456
Martin Henstridge <mchenstridge@gmail.com>
2020-12-19 01:22:03 +0000

    Convert day 18 to use shunting yard algorithm

--- aoc/day18.py
+++ aoc/day18.py
@@ -1,72 +1,65 @@
-import operator
 from . import util
 
 
 def get_expressions(lines):
-    return [parse(line.replace(" ", "")) for line in lines]
+    return [line.replace(" ", "") for line in lines]
 
 
-def parse(string):
-    expr = []
+def shunting_yard_algo(tokens, precedence):
+    output = []
+    opstack = []
 
-    while string:
-        char, string = string[0], string[1:]
-        if char == "(":
-            subexpr, string = parse(string)
-            expr.append(subexpr)
-        elif char == ")":
-            return expr, string
-        elif char == "+":
-            expr.append(operator.add)
-        elif char == "*":
-            expr.append(operator.mul)
+    for token in tokens:
+        if token == "(":
+            opstack.append(token)
+        elif token == ")":
+            while opstack and opstack[-1] != "(":
+                op = opstack.pop()
+                output.append(op)
+            opstack.pop()  # <-- should be "("
+        elif token in precedence:
+            while (
+                opstack
+                and opstack[-1] != "("
+                and precedence[opstack[-1]] >= precedence[token]
+            ):
+                op = opstack.pop()
+                output.append(op)
+            opstack.append(token)
         else:
-            expr.append(int(char))
+            output.append(int(token))
 
-    return expr
+    while opstack:
+        op = opstack.pop()
+        output.append(op)
 
-
-def evaluate1(expr):
-    if isinstance(expr, int):
-        return expr
-
-    # Pop off the first value, the rest of the terms are then (operator, value)
-    # pairs.
-    head, expr = expr[0], expr[1:]
-    acc = evaluate1(head)
-
-    while expr:
-        func, term, expr = expr[0], expr[1], expr[2:]
-        acc = func(evaluate1(term), acc)
-
-    return acc
+    return output
 
 
-def evaluate2(expr):
-    if isinstance(expr, int):
-        return expr
+def evaluate(rpn):
+    stack = []
 
-    for idx, term in enumerate(expr):
-        if term is operator.add:
-            # Replace [a, +, b] with [*, *, a + b]. The last index in this slice
-            # may be read as part of a subsequent addition, hence the result of
-            # the addition is stored there. Fill the remaining indices to avoid
-            # resizing the list, use operator.mul since that is already being
-            # filtered out of the final product calculation.
-            expr[idx + 1] = evaluate2(expr[idx - 1]) + evaluate2(expr[idx + 1])
-            expr[idx - 1] = operator.mul
-            expr[idx] = operator.mul
+    for token in rpn:
+        if isinstance(token, int):
+            stack.append(token)
+        elif token == "+":
+            result = stack.pop() + stack.pop()
+            stack.append(result)
+        elif token == "*":
+            result = stack.pop() * stack.pop()
+            stack.append(result)
 
-    # This expression contains no further additions, calculate the product of
-    # everything that's left.
-    return util.product(evaluate2(term) for term in expr if term is not operator.mul)
+    return stack.pop()
 
 
 def run():
     inputlines = util.get_input_lines("18.txt")
     expressions = get_expressions(inputlines)
 
-    total1 = sum(evaluate1(e) for e in expressions)
-    total2 = sum(evaluate2(e) for e in expressions)
+    rpns = [shunting_yard_algo(expr, {"+": 0, "*": 0}) for expr in expressions]
+    total1 = sum(evaluate(rpn) for rpn in rpns)
+
+    rpns = [shunting_yard_algo(expr, {"+": 1, "*": 0}) for expr in expressions]
+    total2 = sum(evaluate(rpn) for rpn in rpns)
 
     return total1, total2
