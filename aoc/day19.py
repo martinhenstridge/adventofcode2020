d7741a28c4c5c976388d3a6bf9fa5ddeb7970cc6
Martin Henstridge <mchenstridge@gmail.com>
2020-12-19 16:53:36 +0000

    Tidier and faster day 19 solution

--- aoc/day19.py
+++ aoc/day19.py
@@ -13,52 +13,44 @@ def get_rules_messages(lines):
             continue
 
         if rule_section:
-            rnum, rule = line.split(": ", maxsplit=1)
-            tokens = [token.replace("\"", "") for token in rule.split()]
-            if "|" in tokens:
-                tokens = ["(?:"] + tokens + [")"]
-            rules[rnum] = tokens
+            key, rule = line.split(": ", maxsplit=1)
+            tokens = [token.replace('"', "") for token in rule.split()]
+            rules[key] = tokens
         else:
             messages.append(line)
 
     return rules, messages
 
 
-def construct_regex(rules):
-    rule0 = rules["0"].copy()
-
-    resolved = False
-    while not resolved:
-        resolved = True
-        for idx, token in enumerate(rule0):
-            if token in rules:
-                rule0[idx:idx+1] = rules[token]
-                resolved = False
-                break
-
-    return "".join(rule0)
+@util.memoize
+def into_regex(key, rules):
+    regex = "".join(into_regex(t, rules) if t in rules else t for t in rules[key])
+    if "|" in rules[key]:
+        regex = "(?:" + regex + ")"
+    return regex
 
 
 def run():
     inputlines = util.get_input_lines("19.txt")
     rules, messages = get_rules_messages(inputlines)
 
-    regex1 = construct_regex(rules)
-    count1 = sum(1 for m in messages if re.fullmatch(regex1, m))
+    # Add variant of rule 8: 42 | 42 8
+    # This means rule 42 repeated n times, where n >= 1.
+    rules["8+"] = ["42", "+"]
 
-    # 8: 42 | 42 8
-    rules["8"] = ["(?:", "42", ")+"]
+    # Add variant of rule 11: 42 31 | 42 11 31
+    # This means rule 42 repeated n times followed by rule 31 repeated n times,
+    # where n >= 1. Trial and error reveals the maximum number of repetitions to
+    # be 4.
+    rules["11+"] = []
+    for n in range(4):
+        rules["11+"] += ["(?:", "42", f"{{{n+1}}}", "31", f"{{{n+1}}}", ")", "|"]
+    rules["11+"].pop()
 
-    # 11: 42 31 | 42 11 31
-    rules["11"] = [
-        "(?:",
-        "(?:", "(?:", "42", "){1}", "(?:", "31", "){1}", ")", "|",
-        "(?:", "(?:", "42", "){2}", "(?:", "31", "){2}", ")", "|",
-        "(?:", "(?:", "42", "){3}", "(?:", "31", "){3}", ")", "|",
-        "(?:", "(?:", "42", "){4}", "(?:", "31", "){4}", ")", ")",
-    ]
+    # Add variant of rule 0, accounting for modified sub-rules.
+    rules["0+"] = [f"{t}+" if t in ("8", "11") else t for t in rules["0"]]
 
-    regex2 = construct_regex(rules)
-    count2 = sum(1 for m in messages if re.fullmatch(regex2, m))
+    count1 = sum(1 for m in messages if re.fullmatch(into_regex("0", rules), m))
+    count2 = sum(1 for m in messages if re.fullmatch(into_regex("0+", rules), m))
 
     return count1, count2
