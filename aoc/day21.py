8229a162b178c20552fff029cb3997f310b15215
Martin Henstridge <mchenstridge@gmail.com>
2020-12-21 11:00:26 +0000

    Day 21 basic parsing

--- aoc/day21.py
+++ aoc/day21.py
@@ -1,6 +1,13 @@
 from . import util
 
 
+def get_foods(lines):
+    for line in lines:
+        ingredients, allergens = line[:-1].split(" (contains ")
+        yield ingredients.split(" "), allergens.split(", ")
+
+
 def run():
     inputlines = util.get_input_lines("21.txt")
+    foods = [f for f in get_foods(inputlines)]
     return ()
