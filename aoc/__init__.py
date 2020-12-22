8d7607e46c8dbfbe469d266dbcff19c148a01875
Martin Henstridge <mhenstri@cisco.com>
2020-12-22 02:01:26 -0800

    Day 22 input, and templates for remaining days

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -20,6 +20,10 @@ from . import (
     day19,
     day20,
     day21,
+    day22,
+    day23,
+    day24,
+    day25,
 )
 
 DAYS = {
@@ -44,4 +48,8 @@ DAYS = {
     "19": day19,
     "20": day20,
     "21": day21,
+    "22": day22,
+    #"23": day23,
+    #"24": day24,
+    #"25": day25,
 }
