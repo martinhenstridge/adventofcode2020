a7302bee3958cfa2a0d7c27f9b9710659a2ed691
Martin Henstridge <mchenstridge@gmail.com>
2020-12-24 09:22:27 +0000

    Lower overhead data structure for day 23

--- aoc/day23.py
+++ aoc/day23.py
@@ -1,77 +1,71 @@
 from . import util
 
 
-class Cup:
-    __slots__ = "val", "next"
-
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
-
 def get_cups(lines):
     return [int(cup) for cup in lines[0]]
 
 
-def construct_ring(vals):
-    cups = [None] * len(vals)
-    tail = Cup(None)
+def construct_ring(vals, length):
+    cups = [0] * length
 
-    # Connect cups together in a linked list.
-    for val in vals:
-        cup = Cup(val)
-        cups[val] = cup
-        tail.next = cup
-        tail = cup
+    # Connect cups together in a linked list, the value is the index of the next
+    # entry in the list.
+    head = vals[0]
+    prev = head
+    for curr in vals[1:]:
+        cups[prev] = curr
+        prev = curr
+    for curr in range(len(vals), length):
+        cups[prev] = curr
+        prev = curr
 
     # Connect the tail around to the head to make a ring.
-    head = cups[vals[0]]
-    tail.next = head
+    cups[prev] = head
+
     return cups, head
 
 
-def get_cups_after(cups, val, count):
+def get_following_cups(cups, count):
     result = []
 
-    cup = cups[val]
+    cup = 0
     for _ in range(count):
-        cup = cup.next
-        result.append(cup.val)
+        cup = cups[cup]
+        result.append(cup)
 
     return result
 
 
-def play(vals, turns, retcount):
-    size = len(vals)
-    cups, curr = construct_ring(vals)
+def play(cups, first, turns):
+    size = len(cups)
+    curr = first
 
     for _ in range(turns):
         # Pick out the following three cups. Leave them connected up for now -
         # they may not actually need to be moved.
         picked_cups = []
-        picked_head = curr.next
+        picked_head = cups[curr]
         picked_tail = curr
         for _ in range(3):
-            picked_tail = picked_tail.next
-            picked_cups.append(picked_tail.val)
+            picked_tail = cups[picked_tail]
+            picked_cups.append(picked_tail)
 
         # Decide where to re-insert them.
-        val = (curr.val - 1) % size
-        while val in picked_cups:
-            val = (val - 1) % size
-        dest = cups[val]
+        dest = (curr - 1) % size
+        while dest in picked_cups:
+            dest = (dest - 1) % size
 
         # Re-insert cups, noting that work is only required in the case that
         # they're actually being moved from their starting position.
-        if dest.val != curr.val:
-            curr.next = picked_tail.next
-            picked_tail.next = dest.next
-            dest.next = picked_head
+        if dest != curr:
+            cups[curr] = cups[picked_tail]
+            cups[picked_tail] = cups[dest]
+            cups[dest] = picked_head
 
         # Move around the ring to the next cup.
-        curr = curr.next
+        curr = cups[curr]
 
-    return get_cups_after(cups, 0, retcount)
+    return cups
 
 
 def run():
@@ -79,13 +73,15 @@ def run():
     cups = get_cups(inputlines)
 
     # Shift cup values by -1 to allow use of modulo arithmetic.
-    starting = [c - 1 for c in cups]
-    finished = play(starting, 100, len(cups) - 1)
-    result1 = int("".join([str(c + 1) for c in finished]))
+    starting, first = construct_ring([c - 1 for c in cups], len(cups))
+    finished = play(starting, first, 100)
+    following = get_following_cups(finished, len(starting) - 1)
+    result1 = int("".join([str(c + 1) for c in following]))
 
     # Ditto shift by -1, plus expand to 1 million cups.
-    starting = [c - 1 for c in cups] + list(range(len(cups), 1000000))
-    finished = play(starting, 10000000, 2)
-    result2 = (finished[0] + 1) * (finished[1] + 1)
+    starting, first = construct_ring([c - 1 for c in cups], 1000000)
+    finished = play(starting, first, 10000000)
+    following = get_following_cups(finished, 2)
+    result2 = util.product([c + 1 for c in following])
 
     return result1, result2
